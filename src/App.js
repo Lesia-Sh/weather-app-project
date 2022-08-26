98729852c13f1191be4554178cd3eb83cb5616b5
Lesia-Sh <shvch.lv@gmail.com>
2022-08-26 18:26:03 -0400

    Added bootsrap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://meek-malabi-7397e3.netlify.app/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Lesia Shevchuk{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/Lesia-Sh/weather-app-project"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://meek-malabi-7397e3.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Lesia Shevchuk{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/Lesia-Sh/weather-app-project"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
