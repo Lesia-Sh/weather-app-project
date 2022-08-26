6710195d27468fb8f2c3b0348a7ce8c445c3d46f
Lesia-Sh <shvch.lv@gmail.com>
2022-08-26 16:10:42 -0400

    Added a github ripo Url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://app.netlify.com/sites/meek-malabi-7397e3/overview"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Lesia Shevchuk{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/Lesia-Sh/weather-app-project"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
